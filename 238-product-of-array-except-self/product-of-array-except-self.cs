b43e14c41a5261ed7101dde4f7c7f62db7be40da
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2025-07-25 00:23:13 +0200

    Time: 1 ms (100.00%) | Memory: 67.2 MB (41.11%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cs
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -7,7 +7,7 @@ public class Solution {
         answer[0] = prefix;
 
         for(int i = 1; i < nums.Length; i++){
-            answer[i] = nums[i - 1] * answer[i - 1];
+            answer[i] = answer[i - 1] * nums[i - 1] ;
         }
 
         for(int i = nums.Length - 1; i >= 0; i--){
