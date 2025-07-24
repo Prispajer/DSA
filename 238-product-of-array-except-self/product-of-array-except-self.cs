c914f850f2a58d539ed69d01ad8e13cbb00f972f
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2025-07-25 00:20:38 +0200

    Time: 5 ms (11.56%) | Memory: 67 MB (52.58%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int[] answer = new int[nums.Length];
+        int prefix = 1;
+        int suffix = 1;
+
+        answer[0] = prefix;
+
+        for(int i = 1; i < nums.Length; i++){
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+
+        for(int i = nums.Length - 1; i >= 0; i--){
+            answer[i] = answer[i] * suffix;
+            suffix = suffix * nums[i];
+        }
+
+        return answer;
+    }
+}
