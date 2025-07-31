84b63eeac1c7d5e2903df4da8c73687107170ee0
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2025-07-31 21:58:47 +0200

    Time: 44 ms (7.71%) | Memory: 58.4 MB (78.02%) - LeetSync

--- 283-move-zeroes/move-zeroes.cs
+++ 283-move-zeroes/move-zeroes.cs
@@ -3,34 +3,29 @@ public class Solution {
         int firstNumber = 0;
         int firstZero = 0; 
         int read = 0;
-
-        // Usuwam warunek if, bo ogranicza przypadki
-        while(read < nums.Length){
-            // Szukamy pierwszego zera od lewej
-            while(firstZero < nums.Length && nums[firstZero] != 0){
+ 
+            while(read < nums.Length){
+                   while(firstZero < nums.Length && nums[firstZero] != 0){
                 firstZero++;
             }
 
-            // Szukamy liczby ≠ 0, która jest po zerze
-            firstNumber = firstZero + 1;
-            while(firstNumber < nums.Length && nums[firstNumber] == 0){
-                firstNumber++;
-            }
-
-            // Jeśli nie znaleziono już niezerowego – przerwij
-            if (firstNumber >= nums.Length || firstZero >= nums.Length) {
+                firstNumber = firstZero + 1;
+                while(firstNumber < read && nums[firstNumber] == 0){
+                    firstNumber++;
+                }
+                
+                if (firstNumber >= nums.Length || firstZero >= nums.Length) {
                 break;
+                }
+                        
+                Swapper(nums, firstZero, firstNumber);
+                read++;
             }
-
-            // Zamiana miejscami
-            Swapper(nums, firstZero, firstNumber);
-            
-            // Przesuwamy read (niekoniecznie potrzebny, ale zostawiam zgodnie z Twoją logiką)
-            read++;
-        }
+        
 
         void Swapper(int[] arr, int left, int right){
-            int temp = arr[left];
+            int temp = 0;
+            temp = arr[left];
             arr[left] = arr[right];
             arr[right] = temp;    
         }
